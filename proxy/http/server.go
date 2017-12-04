3bf49a46ca63f45abd638abe177c7942b1539ca9
cnperi <cnperi@users.noreply.github.com>
2017-12-04 16:36:25 +0800

    修正 Http 代理错误处理，解决认证问题
    基于 @sunshineplan 的 #758 commit，解决：
    1、客户端没有提供认证信息时，返回 407 错误，要求按 basic 认证提供信息；
    2、客户端提供的用户名密码无匹配时，返回 403 错误，拒绝访问。

--- proxy/http/server.go
+++ proxy/http/server.go
@@ -121,11 +121,11 @@ Start:
 	if len(s.config.Accounts) > 0 {
 		user, pass, ok := parseBasicAuth(request.Header.Get("Proxy-Authorization"))
 		if !ok {
-			_, err := conn.Write([]byte("HTTP/1.1 407 Proxy Authentication Required\r\n\r\n"))
+			_, err := conn.Write([]byte("HTTP/1.1 407 Proxy Authentication Required\r\n\r\nProxy-Authenticate: Basic realm=\"Access to internal site\"\n"))
 			return err
 		}
 		if !s.config.HasAccount(user, pass) {
-			_, err := conn.Write([]byte("HTTP/1.1 401 UNAUTHORIZED\r\n\r\n"))
+			_, err := conn.Write([]byte("HTTP/1.1 403 Forbidden\r\n\r\n"))
 			return err
 		}
 	}
